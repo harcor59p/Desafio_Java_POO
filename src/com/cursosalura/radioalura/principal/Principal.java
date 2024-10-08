83eca3e3bb7a0170920b9d45b558058370b66b24
Harold Cortes <harcor59p@gmail.com>
2024-10-08 13:34:52 -0500

    instanciando las clases

--- src/com/cursosalura/radioalura/principal/Principal.java
+++ src/com/cursosalura/radioalura/principal/Principal.java
@@ -1,6 +1,7 @@
 package com.cursosalura.radioalura.principal;
 
 import com.aluracursos.radioalura.modelos.Cancion;
+import com.aluracursos.radioalura.modelos.Podcast;
 
 public class Principal {
     public static void main(String[] args) {
@@ -10,6 +11,23 @@ public class Principal {
         miCancion.setCantante("Julio Jaramillo");
         miCancion.setGenero("Popular");
         miCancion.setDuracionEnMinutos(4.15) ;
+
+        Podcast miPodcast = new Podcast() ;
+        miPodcast.setDescripcion("Feminismo del Siglo XXI");
+        miPodcast.setTitulo("Feminismo");
+        miPodcast.setDuracionEnMinutos(120);
+
+        for (int i = 0; i < 100; i++) {
+            miCancion.meGusta();
+
+        }
+
+        for (int i = 0; i < 5000 ; i++) {
+            miCancion.reproduce();
+
+        }
+        System.out.println("Total de Reproducciones " + miCancion.getTotalDeReproducciones());
+        System.out.println("Total de Me Gusta " + miCancion.getTotalDeMeGusta());
     }
 
 }
